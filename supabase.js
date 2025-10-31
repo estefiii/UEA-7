0553563d7c13b3c31fa1bb734fd8a556738bd936
estefiii <2233072978@correo.ler.uam.mx>
2025-10-31 09:51:55 -0600

    supa

---
+++ supabase.js
@@ -0,0 +1,4 @@
+//import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://kfqidzjgmsutzinomkza.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmcWlkempnbXN1dHppbm9ta3phIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5MTQ4NTAsImV4cCI6MjA3NzQ5MDg1MH0.eK749P3ZAfaUW9IFApWXMc1wdVxUAYuVLL0ZkgsLh8s')
